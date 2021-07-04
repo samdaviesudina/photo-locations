fee53304593e54a072b39b69d2b931c852b354cf
samdaviesudina <sam.d@brainlabsdigital.com>
2021-07-04 12:01:59 +0100

    Clean up a bit

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import os
 from app.image import ImageFile, ImageFiles, UnsupportedImageFormatError
 from app.location import InvalidLocationError, Location, World
 
-API_KEY = "SV6kWr3TZFpCnj0_kFDsYCmeBxiCSDY5WsZp4WPtUsE"
-
 IMAGES_DIRECTORY = "images"
 
 
@@ -14,7 +12,7 @@ def main() -> None:
     image_files = ImageFiles(IMAGES_DIRECTORY).get()
     print(f"Processing {len(image_files)} images...")
 
-    world = World(API_KEY)
+    world = World(get_here_api_key())
 
     problematic_images = []
     located_images: defaultdict = defaultdict(list)
@@ -28,24 +26,42 @@ def main() -> None:
             if counter % 100 == 0:
                 print(f"Have processed {counter}.")
         except UnsupportedImageFormatError:
-            problematic_images.append(image_file.name)
+            problematic_images.append(image_file)
         except InvalidLocationError:
-            problematic_images.append(image_file.name)
+            problematic_images.append(image_file)
 
     print("Finished processing all the images.")
     print(f"There were {len(problematic_images)} problematic images.")
+    directory_name = f"{IMAGES_DIRECTORY}/problematic_images"
+    if not os.path.isdir(directory_name):
+        os.mkdir(directory_name)
+    for problematic_image in problematic_images:
+        os.rename(
+            f"{IMAGES_DIRECTORY}/{problematic_image.name}",
+            f"{IMAGES_DIRECTORY}/problematic_images/{problematic_image.name}",
+        )
 
     print("Organising images into folders...")
     for city in located_images.keys():
         city_snake_case = city.replace(" ", "_")
-        os.mkdir(f"images/{city_snake_case}")
+
+        directory_name = f"{IMAGES_DIRECTORY}/{city_snake_case}"
+        if not os.path.isdir(directory_name):
+            os.mkdir(directory_name)
         for image in located_images[city]:
             os.rename(
                 image.image_file.filepath,
-                f"images/{city_snake_case}/{image.image_file.name}",
+                f"{IMAGES_DIRECTORY}/{city_snake_case}/{image.image_file.name}",
             )
 
     print("Done.")
+
+
+def get_here_api_key() -> str:
+    try:
+        return os.environ["HERE_API_KEY"]
+    except KeyError as e:
+        raise Exception("Please export the environment variable 'HERE_API_KEY'.") from e
 
 
 @dataclass
